5d1f74a67a0b5c5f7f96605f54cb13fa370aef94
Andre Luis <andreluisaraujo25@hotmail.com>
2024-07-12 13:05:59 -0300

    Jogada Especial Promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -43,6 +43,12 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Digite a peca para promocao (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
